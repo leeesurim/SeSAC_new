a6e2d670d09e096c60ede75daed6d8318085ea29
leeesurim <12esuqe@gmail.com>
2022-08-22 21:44:01 +0900

    study: project

--- nodejs/memo/controller/MemoController.js
+++ nodejs/memo/controller/MemoController.js
@@ -1,3 +1,9 @@
 exports.index = (req,res) => {
     res.render("index");
 }
+
+exports.post_cotent = (req, res) => {
+    console.log( req.body );
+    res.send(req.body);
+
+}
--- nodejs/memo/routes/index.js
+++ nodejs/memo/routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const memo = require("../controller/MemoController");
 
 router.get("/", memo.index); 
+router.post("/write", memo.post_cotent);
 
 module.exports = router; 
