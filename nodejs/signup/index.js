3d6bb873e92ca49a4a2ac5a093ceebb9b4264df7
leesurim <12esuq4@gmail.com>
2022-08-28 02:02:34 +0900

    feat: signup

--- nodejs/signup/index.js
+++ nodejs/signup/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const bodyParser = require("body-parser");
-const path = require('path');
 
 app.set('views', './src/views/ejs')
 app.set("view engine", "ejs");
@@ -14,6 +13,14 @@ app.use( bodyParser.json() );
 app.get("/signup", function(req, res){
     res.render("signup");
 });
+
+app.post("/signup", function(req, res){
+    res.send(req.body);
+})
+
+app.get("/login", function(req, res){
+    res.render("login");
+})
 
 app.listen(port, ()=>{
     console.log( "Server Port : ", port );
